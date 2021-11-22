50d0c90202ff91be15f358510314aaa0394753fa
Ubuntu <ubuntu@ip-172-31-82-219.ec2.internal>
2021-11-22 19:57:30 +0000

    package info

---
+++ package-info.java
@@ -0,0 +1,10 @@
+/**
+* This program gets the length of logs from the user's input,
+* calculates and display how much logs the truck can carry to the output.
+*
+* @author  Donggeun Lim
+* @version 1.0
+* @since   2021-11-20
+*/
+package ca.mths.unit1.unit01.java.main;
+
